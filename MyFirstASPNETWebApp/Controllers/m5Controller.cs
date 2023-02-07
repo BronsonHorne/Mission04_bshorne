397efa0df2979b2af53f879157712da5b60f5bcd
Bronson Horne <bronsonhorne@gmail.com>
2023-02-06 22:01:32 -0700

    Mission 5 assignment

---
+++ MyFirstASPNETWebApp/Controllers/m5Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission04_bshorne.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_bshorne.controllers
+{
+    public class m5Controller : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalc ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc (GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstASPNETWebApp/Models/GradeCalcModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_bshorne.Models
+{
+    public class GradeCalcModel
+    {
+        [Required]
+        [Range(0, 100)]
+        public float Assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float group { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float quiz { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float mid { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float final { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float intex { get; set; }
+    }
+}
